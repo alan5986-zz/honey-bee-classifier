e12d866864b733b7987aed8fe7a8e7bf2fdc3a62
alan5986 <51156343+alan5986@users.noreply.github.com>
2019-06-21 12:28:09 +0100

    add capture scripts

---
+++ camera scripts/capture.py
@@ -0,0 +1,26 @@
+#Takes continuous captures with Pi camera and saves to USB drive
+import time
+import picamera
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (1920, 1080)
+    #camera.framerate = 120
+    camera.shutter_speed = 2000
+    # Wait for the automatic gain control to settle
+    time.sleep(3)
+    # Now fix the values
+    #camera.shutter_speed = camera.exposure_speed
+    camera.exposure_mode = 'off'
+    g = camera.awb_gains
+    camera.awb_mode = 'off'
+    camera.awb_gains = g
+    print(camera.shutter_speed)
+    # Finally, take several photos with the fixed settings
+    #print(camera.exposure_speed)
+    for i, filename in enumerate(camera.capture_continuous('/media/pi/USB/rawimages/image{timestamp:%d-%m-%Y-%H-%M-%S-%f}.jpg')):
+        #seconds between each capture
+        time.sleep(0.5)
+        #counter, use i == 3600 with 1 second sleep for 1 hour of capture
+        if i == 20:
+            break
+
