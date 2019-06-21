7ad6c3a1aa9c8de607ab4eb5efc0f5fc6c7e25e6
alan5986 <51156343+alan5986@users.noreply.github.com>
2019-06-21 12:27:13 +0100

    update dimensions and path

--- camera scripts/crop.py
+++ camera scripts/crop.py
@@ -3,12 +3,12 @@ import cv2
 
 #Read all images from one directory, crop them and save them to another directory
 
-readFolder = 'C:' + os.sep + os.path.join('Users', 'Sam', 'Documents', 'MastersFolder', 'Dissertation', 'labelledimages')
-writeFolder = 'C:' + os.sep + os.path.join('Users', 'Sam', 'Documents', 'MastersFolder', 'Dissertation', 'croppedimages')
+readFolder = 'E:' + os.sep + os.path.join('rawimages')
+writeFolder = 'E:' + os.sep + os.path.join('croppedimages')
 
 allImages = [f for f in os.listdir(readFolder) if os.path.isfile(os.path.join(readFolder, f))]
 
 for img in allImages:
     readImg = cv2.imread(os.path.join(readFolder, img))
-    crop_img = readImg[0:1080, 400:1050]
+    crop_img = readImg[120:955, 540:1355]
     cv2.imwrite(os.path.join(writeFolder, img), crop_img)
